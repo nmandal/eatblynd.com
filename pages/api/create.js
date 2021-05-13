c7a2af7bcf20a64e5aa6fe90b07ed247a62b4bd5
nmandal <nicholas.mandal@gmail.com>
2021-05-13 15:26:03 -0500

    amend character minumum to three

--- pages/api/create.js
+++ pages/api/create.js
@@ -5,12 +5,12 @@ import { DB_NAME, FEATURE_TYPE } from '../../lib/const'
 export default authenticate(async (req, res) => {
   const { title } = req.body
 
-  let schema = string().required().trim().min(10).max(70)
+  let schema = string().required().trim().min(3).max(70)
   const isValid = await schema.isValid(title)
   if (!isValid)
     return res
       .status(400)
-      .json({ error: 'Min 10 and Max 70 characters please.' })
+      .json({ error: 'Min 3 and Max 70 characters please.' })
 
   const { nickname, email, updated_at, ...user } = req.user
   const feature = {
