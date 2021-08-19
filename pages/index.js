b322cc638e07f36ed4a57003d570a46603b2a479
nmandal <nicholas.mandal@gmail.com>
2021-08-19 14:12:42 -0500

    title

--- pages/index.js
+++ pages/index.js
@@ -455,7 +455,7 @@ export default function Example() {
 
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
-            <h1 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline">Order blindly from your</span>{' '}
               <span className="block text-cyan-600 xl:inline">favorite restaurants</span>
             </h1>
