ede4540d21aa7b9aadc3718dfd04abb91d071944
nmandal <nicholas.mandal@gmail.com>
2021-08-19 08:36:04 -0500

    change font

--- pages/index.js
+++ pages/index.js
@@ -390,8 +390,8 @@ export default function Example() {
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Order blindly from your</span>{' '}
-              <span className="block text-cyan-600 xl:inline">favorite restaurants</span>
+              <span className="block l:inline">Order blindly from your</span>{' '}
+              <span className="block text-cyan-600 l:inline">favorite restaurants</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Restaurants serve one dish at a time. It's unknown to customers and can change at any time. Blynd invites you to experience local cuisine without the hassle.
