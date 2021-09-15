9d2c1db1adb48021b50413a082f0a3bf96d32013
nmandal <nicholas.mandal@gmail.com>
2021-09-15 12:01:22 -0500

    wow

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,28 +1,23 @@
 import FormData from 'form-data'
-import fetch from 'node-fetch'
 	
 const url = 'https://www.getrevue.co/api/v2/subscribers'
 const secret = process.env.REVUE_SECRET_KEY
 	
 export default async function (req, res) {
   console.log(req.body)
-  const { email } = JSON.parse(req.body)
+  const { email } = req.body
 	
   if (!email) {
     res.status(400).json({ error: 'No email submitted' })
     return
   }
 	
-  const formData = new FormData()
-  formData.append('email', email)
-  formData.append('double_opt_in', 'false')
-	
   const result = await fetch(url, {
     method: 'POST',
     headers: {
       Authorization: `Token ${secret}`
     },
-    body: formData
+    body: JSON.stringify({email: email})
   })
 
   console.log(result)
