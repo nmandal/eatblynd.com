3a841833a4df7961fda120d95b4c4c02bb562717
nmandal <nicholas.mandal@gmail.com>
2021-09-15 12:28:28 -0500

    copy

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,31 +1,45 @@
-const url = 'https://www.getrevue.co/api/v2/subscribers'
-const secret = process.env.REVUE_SECRET_KEY
-	
-export default async function (req, res) {
-  console.log(req.body)
-  const { email } = req.body
-  console.log(email)
-	
+export default async function handler(req, res) {
+  // 1. Get the email from the payload and
+  // validate if it is empty.
+  const { email } = req.body;
   if (!email) {
-    res.status(400).json({ error: 'No email submitted' })
-    return
+      return res.status(400).json({error: 'No email submitted.'});
   }
-	
-  const result = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${secret}`
-    },
-    body: JSON.stringify({email})
-  })
 
-  console.log(result)
-  const data = await result.json()
-	
-  if (!result.ok) {
-    res.status(500).json({ error: data.error.email[0] })
-    return
+  // 2. Use the Revue API Key and create a subscriber using
+  // the email we pass to the API. Please note, we pass the
+  // API Key in the 'Authorization' header.
+  try {
+      const API_KEY = process.env.REVUE_API_KEY;
+      const response = await fetch(
+          `https://www.getrevue.co/api/v2/subscribers`,
+          {
+              method: 'POST',
+              body: JSON.stringify({email: email}),
+              headers: {
+                  'Authorization': `Token ${API_KEY}`,
+                  'Content-Type': 'application/json'
+              }
+          }
+      )
+  
+  // 3. We check in the response if the status is 400
+  // If so, consider it as error and return. Otherwise a 201
+  // for create        
+      if (response.status >=400) {
+          const message = await response.json();
+          console.log(message.error.email[0]);
+          return res.status(400).json({error: message.error.email[0]});
+      }
+
+      // Send a JSON response
+      res.status(201).json({
+          message: `Hey, ${email}, Please check your email and verify it. Can't wait to get you boarded.`,
+          error: ''
+      });
+  } catch (err) {
+  // 4. If the control goes inside the catch block
+  // let us consider it as a server error(500)  
+      return res.status(500).json({error: err.message || error.toString()});
   }
-	
-  res.status(200).json({ data })
 }
